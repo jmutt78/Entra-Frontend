67fc92190336aafd5323e9255af7c5331d6d5bfa
jmutt78 <jmcintosh@docusites.com>
2019-04-11 13:33:19 -0700

    move next

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import App, { Container } from "next/app";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
 import Page from "../components/page/Page";
 
 import React from "react";
@@ -20,28 +22,39 @@ class MyApp extends App {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
 
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps, apollo } = this.props;
     return (
       <Container>
-        <JssProvider
-          registry={this.pageContext.sheetsRegistry}
-          generateClassName={this.pageContext.generateClassName}
-        >
-          <MuiThemeProvider
-            theme={this.pageContext.theme}
-            sheetsManager={this.pageContext.sheetsManager}
+        <ApolloProvider client={apollo}>
+          <JssProvider
+            registry={this.pageContext.sheetsRegistry}
+            generateClassName={this.pageContext.generateClassName}
           >
-            <CssBaseline />
-            <Page>
-              <Component pageContext={this.pageContext} {...pageProps} />
-            </Page>
-          </MuiThemeProvider>
-        </JssProvider>
+            <MuiThemeProvider
+              theme={this.pageContext.theme}
+              sheetsManager={this.pageContext.sheetsManager}
+            >
+              <CssBaseline />
+              <Page>
+                <Component pageContext={this.pageContext} {...pageProps} />
+              </Page>
+            </MuiThemeProvider>
+          </JssProvider>
+        </ApolloProvider>
       </Container>
     );
   }
 }
 
-export default MyApp;
+export default withData(MyApp);
---
+++ pages/signup.js
@@ -0,0 +1,9 @@
+import Signup from "../components/auth/Signup";
+
+const SignupPage = props => (
+  <div>
+    <Signup />
+  </div>
+);
+
+export default SignupPage;
