34599ba6d1c210db79b49bcc0929701c741b590d
kiran <kiran@neostack.com>
2019-05-19 18:30:45 +0530

    styling

--- components/create-question/index.js
+++ components/create-question/index.js
@@ -5,11 +5,13 @@ import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import { withStyles } from "@material-ui/core/styles";
-import Input from "@material-ui/core/Input";
+import FilledInput from "@material-ui/core/FilledInput";
 import MenuItem from "@material-ui/core/MenuItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import Checkbox from "@material-ui/core/Checkbox";
+import FormControl from "@material-ui/core/FormControl";
 import CreateTag from "./CreateTag";
 import { TAGS_QUERY } from "./Tags";
 
@@ -17,9 +19,30 @@ const styles = theme => ({
   grid: {
     margin: theme.spacing.unit
   },
+  container: {
+    display: "flex"
+  },
+  flex1: {
+    flex: 1
+  },
   root: {
     margin: theme.spacing.unit,
     marginTop: 40
+  },
+  inputField: {
+    width: 700,
+    marginBottom: 30
+  },
+  addNewTag: {
+    marginTop: -15,
+    marginBottom: 30
+  },
+  label: {
+    marginLeft: 10,
+    marginBotom: 10
+  },
+  postQuestionButton: {
+    alignItems: "flex-end"
   }
 });
 
@@ -46,7 +69,7 @@ const MenuProps = {
   PaperProps: {
     style: {
       maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250
+      width: 700
     }
   }
 };
@@ -82,7 +105,6 @@ class CreateQuestion extends React.Component {
   render() {
     const { classes } = this.props;
     const { title, description, tags, showCreateTagModal } = this.state;
-    console.log(this.state);
     return (
       <Query query={TAGS_QUERY}>
         {({ loading, data }) => {
@@ -100,62 +122,124 @@ class CreateQuestion extends React.Component {
             >
               {(createQuestion, { error, loading }) => {
                 return (
-                  <div container className={classes.root} spacing={16}>
-                    <div>
-                      <h1>Ask a question</h1>
-                    </div>
-                    <div>
-                      <label htmlFor="title">
-                        <TextField
-                          label="Title"
-                          type="text"
-                          name="title"
-                          variant="filled"
-                          value={title}
-                          onChange={this.handleTitleChange}
-                          className={classes.textField}
-                        />
-                      </label>
-                      <label htmlFor="description">
-                        <TextField
-                          label="Description"
-                          type="text"
-                          name="description"
-                          variant="filled"
-                          value={description}
-                          onChange={this.handleDescriptionChange}
-                          className={classes.textField}
-                        />
-                      </label>
-                      <Select
-                        multiple
-                        value={tags}
-                        onChange={this.handleTagsChange}
-                        input={<Input id="select-multiple-checkbox" />}
-                        renderValue={selected => selected.join(", ")}
-                        MenuProps={MenuProps}
+                  <Grid container className={classes.root} spacing={16}>
+                    <Grid item xs={2} />
+                    <Grid item xs={6}>
+                      <form
+                        method="post"
+                        onSubmit={async e => {
+                          e.preventDefault();
+                          await createQuestion();
+
+                          this.setState({
+                            title: "",
+                            description: "",
+                            tags: []
+                          });
+                        }}
                       >
-                        {data.tags.map(tag => (
-                          <MenuItem key={tag.name} value={tag.name}>
-                            <Checkbox
-                              checked={this.state.tags.indexOf(tag.name) > -1}
+                        <fieldset
+                          disabled={loading}
+                          aria-busy={loading}
+                          style={{
+                            borderWidth: "0px"
+                          }}
+                        >
+                          <div>
+                            <div>
+                              <h1>Ask a question</h1>
+                            </div>
+                            <div>
+                              <FormControl>
+                                <label htmlFor="title">
+                                  <TextField
+                                    label="Title"
+                                    type="text"
+                                    name="title"
+                                    variant="filled"
+                                    value={title}
+                                    onChange={this.handleTitleChange}
+                                    className={classes.inputField}
+                                  />
+                                </label>
+                              </FormControl>
+
+                              <FormControl
+                                variant="filed"
+                                className={classes.inputField}
+                              >
+                                <InputLabel
+                                  htmlFor="tags"
+                                  className={classes.label}
+                                >
+                                  Tag(s)
+                                </InputLabel>
+                                <Select
+                                  multiple
+                                  value={tags}
+                                  name="tags"
+                                  onChange={this.handleTagsChange}
+                                  input={
+                                    <FilledInput
+                                      name="tab"
+                                      id="filled-age-native-simple"
+                                    />
+                                  }
+                                  renderValue={selected => selected.join(", ")}
+                                  MenuProps={MenuProps}
+                                >
+                                  {data.tags.map(tag => (
+                                    <MenuItem key={tag.name} value={tag.name}>
+                                      <Checkbox
+                                        checked={
+                                          this.state.tags.indexOf(tag.name) > -1
+                                        }
+                                      />
+                                      <ListItemText primary={tag.name} />
+                                    </MenuItem>
+                                  ))}
+                                </Select>
+                              </FormControl>
+                              <Button
+                                variant="text"
+                                onClick={this.openCreateTagModal}
+                                className={classes.addNewTag}
+                              >
+                                ADD NEW TAG
+                              </Button>
+                              <FormControl>
+                                <label htmlFor="description">
+                                  <TextField
+                                    label="Description"
+                                    type="text"
+                                    name="description"
+                                    variant="filled"
+                                    multiline
+                                    rows={4}
+                                    value={description}
+                                    onChange={this.handleDescriptionChange}
+                                    className={classes.inputField}
+                                  />
+                                </label>
+                              </FormControl>
+                            </div>
+                            <CreateTag
+                              open={showCreateTagModal}
+                              onClose={this.closeCreateTagModal}
                             />
-                            <ListItemText primary={tag.name} />
-                          </MenuItem>
-                        ))}
-                      </Select>
-                      <Button variant="link" onClick={this.openCreateTagModal}>
-                        ADD NEW TAG
-                      </Button>
-                    </div>
-                    <CreateTag
-                      open={showCreateTagModal}
-                      onClose={this.closeCreateTagModal}
-                    />
-                    <Button variant="link" onClick={() => createQuestion()}>
-                      Create Question
-                    </Button>
-                  </div>
+                            <div className={classes.container}>
+                              <div className={classes.flex1} />
+                              <div>
+                                <Button variant="contained" type="submit">
+                                  Post Question
+                                </Button>
+                              </div>
+                            </div>
+                          </div>
+                        </fieldset>
+                      </form>
+                    </Grid>
+                  </Grid>
                 );
               }}
             </Mutation>
