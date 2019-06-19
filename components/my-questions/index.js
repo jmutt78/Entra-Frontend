d84598dad443fddcafeb93fb5faa052fa7facbbd
kiran <kiran@neostack.com>
2019-06-19 23:15:07 +0530

    separate question list query

--- components/my-questions/index.js
+++ components/my-questions/index.js
@@ -1,36 +1,14 @@
 import React, { Component } from "react";
 import { Query } from "react-apollo";
-import gql from "graphql-tag";
 import { perPage } from "../../config.js";
 import QuestionList from "../question-list";
-
-const MYQUESTIONS_QUERY = gql`
-  query MYQUESTIONS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
-    questions(first: $first, skip: $skip, orderBy: createdAt_DESC) {
-      id
-      title
-      description
-      createdAt
-      answers {
-        id
-        body
-      }
-      tags {
-        id
-        name
-      }
-      views
-      upVotes
-      downVotes
-    }
-  }
-`;
+import questionListQuery from "../question-list/questionListQuery";
 
 class MyQuestions extends Component {
   render() {
     return (
       <Query
-        query={MYQUESTIONS_QUERY}
+        query={questionListQuery}
         fetchPolicy="network-only"
         variables={{
           skip: this.props.page * perPage - perPage,
