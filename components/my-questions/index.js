2c0a21b50fcc1b5c2e4ebd7133fbfea192274260
kiran <kiran@neostack.com>
2019-06-19 23:09:32 +0530

    sepated question list

--- components/my-questions/index.js
+++ components/my-questions/index.js
@@ -2,18 +2,7 @@ import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import { perPage } from "../../config.js";
-import { withStyles } from "@material-ui/core/styles";
-import { format, parseISO } from "date-fns";
-import Link from "next/link";
-import Pagination from "../pagination";
-
-import Grid from "@material-ui/core/Grid";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
-import Typography from "@material-ui/core/Typography";
+import QuestionList from "../question-list";
 
 const MYQUESTIONS_QUERY = gql`
   query MYQUESTIONS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
@@ -37,50 +26,8 @@ const MYQUESTIONS_QUERY = gql`
   }
 `;
 
-const CustomTableCell = withStyles(theme => ({
-  head: {
-    width: 5
-  }
-}))(TableCell);
-
-const styles = theme => ({
-  grid: {
-    margin: theme.spacing.unit
-  },
-
-  root: {
-    margin: theme.spacing.unit,
-    marginTop: 40
-  },
-
-  title: {
-    fontSize: "40px",
-    textAlign: "Left",
-    color: "rgba(0, 0, 0, 0.87)"
-  },
-  link: {
-    textDecoration: "none",
-    color: "rgba(0, 0, 0, 0.87)"
-  }
-});
-
 class MyQuestions extends Component {
-  tagsList(tags) {
-    return tags.map(tags => (
-      <div key={tags.id} style={{ display: "inline-flex", marginRight: 10 }}>
-        <Typography style={{ textTransform: "uppercase" }}>
-          <strong>{tags.name} </strong>
-        </Typography>
-      </div>
-    ));
-  }
-
   render() {
-    const { classes } = this.props;
-
-    const customColumnStyle = {
-      maxWidth: ".3px"
-    };
     return (
       <Query
         query={MYQUESTIONS_QUERY}
@@ -91,82 +38,13 @@ class MyQuestions extends Component {
         }}
       >
         {({ data: { questions }, loading }) => {
+          console.log(questions);
           if (loading) return <p>Loading...</p>;
-          return (
-            <Grid container className={classes.root} spacing={16}>
-              <Grid item xs={3} className={classes.grid} />
-              <Grid item xs={7} className={classes.grid}>
-                {" "}
-                <Table className={classes.table}>
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>
-                        <Typography
-                          variant="display3"
-                          className={classes.title}
-                        >
-                          My Questions
-                        </Typography>
-                      </TableCell>
-                      <CustomTableCell style={customColumnStyle}>
-                        A
-                      </CustomTableCell>
-                      <CustomTableCell style={customColumnStyle}>
-                        V
-                      </CustomTableCell>
-                      <CustomTableCell style={customColumnStyle}>
-                        U
-                      </CustomTableCell>
-                      <CustomTableCell style={customColumnStyle}>
-                        D
-                      </CustomTableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {questions.map(question => {
-                      return (
-                        <TableRow key={question.id}>
-                          <TableCell component="th" scope="row">
-                            <Typography>
-                              <Link
-                                href={{
-                                  pathname: "/question",
-                                  query: { id: question.id }
-                                }}
-                              >
-                                <a className={classes.link}>
-                                  <div>{question.title}</div>
-                                </a>
-                              </Link>
-                            </Typography>
-                            <Typography>
-                              Posted{" "}
-                              {format(
-                                parseISO(question.createdAt),
-                                "MMMM dd, yyyy"
-                              )}
-                            </Typography>
-                            {this.tagsList(question.tags)}
-                          </TableCell>
-                          <TableCell>{question.answers.length}</TableCell>
-                          <CustomTableCell>{question.views}</CustomTableCell>
-                          <CustomTableCell>{question.upVotes}</CustomTableCell>
-                          <CustomTableCell>
-                            {question.downVotes}
-                          </CustomTableCell>
-                        </TableRow>
-                      );
-                    })}
-                  </TableBody>
-                </Table>
-                <Pagination page={this.props.page} />
-              </Grid>
-            </Grid>
-          );
+          return <QuestionList questions={questions} />;
         }}
       </Query>
     );
   }
 }
 
-export default withStyles(styles)(MyQuestions);
+export default MyQuestions;
---
+++ components/question-list/index.js
@@ -0,0 +1,117 @@
+import React, { Component } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { format, parseISO } from "date-fns";
+import Link from "next/link";
+import Pagination from "../pagination";
+
+import Grid from "@material-ui/core/Grid";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Typography from "@material-ui/core/Typography";
+
+const CustomTableCell = withStyles(theme => ({
+  head: {
+    width: 5
+  }
+}))(TableCell);
+
+const styles = theme => ({
+  grid: {
+    margin: theme.spacing.unit
+  },
+
+  root: {
+    margin: theme.spacing.unit,
+    marginTop: 40
+  },
+
+  title: {
+    fontSize: "40px",
+    textAlign: "Left",
+    color: "rgba(0, 0, 0, 0.87)"
+  },
+  link: {
+    textDecoration: "none",
+    color: "rgba(0, 0, 0, 0.87)"
+  }
+});
+
+class MyQuestions extends Component {
+  tagsList(tags) {
+    return tags.map(tags => (
+      <div key={tags.id} style={{ display: "inline-flex", marginRight: 10 }}>
+        <Typography style={{ textTransform: "uppercase" }}>
+          <strong>{tags.name} </strong>
+        </Typography>
+      </div>
+    ));
+  }
+
+  render() {
+    const { classes, questions } = this.props;
+
+    const customColumnStyle = {
+      maxWidth: ".3px"
+    };
+    return (
+      <Grid container className={classes.root} spacing={16}>
+        <Grid item xs={3} className={classes.grid} />
+        <Grid item xs={7} className={classes.grid}>
+          {" "}
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <Typography variant="display3" className={classes.title}>
+                    My Questions
+                  </Typography>
+                </TableCell>
+                <CustomTableCell style={customColumnStyle}>A</CustomTableCell>
+                <CustomTableCell style={customColumnStyle}>V</CustomTableCell>
+                <CustomTableCell style={customColumnStyle}>U</CustomTableCell>
+                <CustomTableCell style={customColumnStyle}>D</CustomTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {questions.map(question => {
+                return (
+                  <TableRow key={question.id}>
+                    <TableCell component="th" scope="row">
+                      <Typography>
+                        <Link
+                          href={{
+                            pathname: "/question",
+                            query: { id: question.id }
+                          }}
+                        >
+                          <a className={classes.link}>
+                            <div>{question.title}</div>
+                          </a>
+                        </Link>
+                      </Typography>
+                      <Typography>
+                        Posted{" "}
+                        {format(parseISO(question.createdAt), "MMMM dd, yyyy")}
+                      </Typography>
+                      {this.tagsList(question.tags)}
+                    </TableCell>
+                    <TableCell>{question.answers.length}</TableCell>
+                    <CustomTableCell>{question.views}</CustomTableCell>
+                    <CustomTableCell>{question.upVotes}</CustomTableCell>
+                    <CustomTableCell>{question.downVotes}</CustomTableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+          <Pagination page={this.props.page} />
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(MyQuestions);
