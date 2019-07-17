f4f3bfe61bb98d9f48b2db5f81a3799b993082a2
kiran <kiran@neostack.com>
2019-07-17 18:23:41 +0530

    use route hook to get path name

--- components/pagination/index.js
+++ components/pagination/index.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import Link from "next/link";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 import { perPage } from "../../config.js";
 import { withStyles } from "@material-ui/core/styles";
@@ -27,55 +28,58 @@ const PAGINATION_QUERY = gql`
   }
 `;
 
-const Pagination = props => (
-  <Query query={PAGINATION_QUERY}>
-    {({ data, loading, error }) => {
-      if (loading) return <p>Loading...</p>;
-      if (error) return <Error error={error} />;
-      console.log(data.questionsConnection.aggregate.count);
-      const count = data.questionsConnection.aggregate.count;
-      const pages = Math.ceil(count / perPage);
-      const page = props.page;
+function Pagination(props) {
+  const router = useRouter();
+  return (
+    <Query query={PAGINATION_QUERY}>
+      {({ data, loading, error }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <Error error={error} />;
+        console.log(data.questionsConnection.aggregate.count);
+        const count = data.questionsConnection.aggregate.count;
+        const pages = Math.ceil(count / perPage);
+        const page = props.page;
 
-      return (
-        <div>
-          <Head>
-            <title>
-              Entra — Page {page} of {pages}
-            </title>
-          </Head>
-          <div align="right">
-            <Typography style={{ display: "inline-flex" }}>
-              Page {page} - {pages}
-            </Typography>
-            <Link
-              prefetch
-              href={{
-                pathname: window.location.pathname,
-                query: { page: page - 1 }
-              }}
-            >
-              <IconButton disabled={page <= 1}>
-                <KeyboardArrowLeft />
-              </IconButton>
-            </Link>
-            <Link
-              prefetch
-              href={{
-                pathname: window.location.pathname,
-                query: { page: page + 1 }
-              }}
-            >
-              <IconButton disabled={page >= pages}>
-                <KeyboardArrowRight />
-              </IconButton>
-            </Link>
+        return (
+          <div>
+            <Head>
+              <title>
+                Entra — Page {page} of {pages}
+              </title>
+            </Head>
+            <div align="right">
+              <Typography style={{ display: "inline-flex" }}>
+                Page {page} - {pages}
+              </Typography>
+              <Link
+                prefetch
+                href={{
+                  pathname: router.pathname,
+                  query: { page: page - 1 }
+                }}
+              >
+                <IconButton disabled={page <= 1}>
+                  <KeyboardArrowLeft />
+                </IconButton>
+              </Link>
+              <Link
+                prefetch
+                href={{
+                  pathname: router.pathname,
+                  query: { page: page + 1 }
+                }}
+              >
+                <IconButton disabled={page >= pages}>
+                  <KeyboardArrowRight />
+                </IconButton>
+              </Link>
+            </div>
           </div>
-        </div>
-      );
-    }}
-  </Query>
-);
+        );
+      }}
+    </Query>
+  );
+}
 
 export default Pagination;
 export { PAGINATION_QUERY };
