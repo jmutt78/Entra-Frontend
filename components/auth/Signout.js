00b3de4109d9a43e2ac90750444ccf4b7e78e19a
jmutt78 <jmcintosh@docusites.com>
2019-07-24 12:32:58 -0700

    fixed signout

--- components/auth/Signout.js
+++ components/auth/Signout.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import Button from "@material-ui/core/Button";
-import Link from "next/link";
+
 import Router from "next/router";
 import { CURRENT_USER_QUERY } from "./User";
 
@@ -14,25 +14,36 @@ const SIGN_OUT_MUTATION = gql`
   }
 `;
 
-function push(e) {
-  e.preventDefault();
-  Router.push("/");
+class Signout extends React.Component {
+  handleSignout = async (e, signout) => {
+    e.preventDefault();
+    const res = await signout();
+
+    console.log("Updated!!");
+    Router.push({
+      pathname: "/"
+    });
+  };
+  render() {
+    return (
+      <Mutation
+        mutation={SIGN_OUT_MUTATION}
+        refetchQueries={[{ query: CURRENT_USER_QUERY }]}
+      >
+        {(signout, { error, loading }) => {
+          return (
+            <Button
+              style={{ marginLeft: "2rem" }}
+              color="default"
+              onClick={e => this.handleSignout(e, signout)}
+            >
+              Sign Out
+            </Button>
+          );
+        }}
+      </Mutation>
+    );
+  }
 }
 
-const Signout = props => (
-  <Mutation
-    mutation={SIGN_OUT_MUTATION}
-    refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-  >
-    {signout => (
-      <Button
-        style={{ marginLeft: "2rem" }}
-        color="default"
-        onClick={e => signout(e, push)}
-      >
-        Sign Out
-      </Button>
-    )}
-  </Mutation>
-);
 export default Signout;
