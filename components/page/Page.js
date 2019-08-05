eebb6fa521003c2be65a2631b371affea8a1793b
Felix Volny <github@volny.co>
2019-08-05 19:47:55 +0700

    main layout

--- components/page/Page.js
+++ components/page/Page.js
@@ -1,19 +1,32 @@
-import React, { Component } from "react";
-import Header from "../header/Header.js";
-import Meta from "../meta/Meta.js";
-import Footer from "../footer";
+import React from 'react'
+import Header from '../header/Header.js'
+import Meta from '../meta/Meta.js'
+import Footer from '../footer'
+import { makeStyles } from '@material-ui/core/styles'
 
-class Page extends Component {
-  render() {
-    return (
-      <div>
-        <Meta />
-        <Header />
-        {this.props.children}
-        <Footer />
-      </div>
-    );
-  }
+const styles = {
+  mainLayout: {
+    width: '100vw',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  contentContainer: {
+    flex: 1,
+  },
 }
 
-export default Page;
+const Page = ({ children }) => {
+  const { mainLayout, contentContainer } = styles
+
+  return (
+    <div style={mainLayout}>
+      <Meta />
+      <Header />
+      <div style={contentContainer}>{children}</div>
+      <Footer />
+    </div>
+  )
+}
+
+export default Page
