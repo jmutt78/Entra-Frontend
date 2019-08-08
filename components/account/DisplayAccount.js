e096311929e8b60764b8872d948e1f412c09daf6
Felix Volny <github@volny.co>
2019-08-08 16:41:22 +0700

    layout and header for my account page

--- components/account/DisplayAccount.js
+++ components/account/DisplayAccount.js
@@ -1,51 +1,71 @@
-import React, { Component } from "react";
-import { Query } from "react-apollo";
-import QaDisplay from "./QaDisplay";
-import MainInfoDisplay from "./MainInfoDisplay";
-import BadgesDisplay from "./BadgesDisplay";
-import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
+import React, { Component } from 'react'
+import { Query } from 'react-apollo'
 
-import { CURRENT_USER_QUERY } from "../auth/User";
+import Grid from '@material-ui/core/Grid'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+
+import BadgesDisplay from './BadgesDisplay'
+import MainInfoDisplay from './MainInfoDisplay'
+import QaDisplay from './QaDisplay'
+
+import { CURRENT_USER_QUERY } from '../auth/User'
 
 const styles = theme => ({
-  grid: {
-    margin: theme.spacing.unit
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
   },
-  root: {
-    margin: theme.spacing.unit,
-    marginTop: 40
-  }
-});
+  title: {
+    fontSize: '40px',
+    textAlign: 'Left',
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+  link: {
+    textDecoration: 'none',
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+  icon: {
+    color: 'black',
+  },
+})
 
 class DisplayAccount extends Component {
   render() {
-    const { classes } = this.props;
+    const { classes } = this.props
     return (
       <Query query={CURRENT_USER_QUERY}>
         {({ data, loading }) => {
-          if (loading) return <p>Loading...</p>;
-          const user = data.me;
+          if (loading) return <p>Loading...</p>
+          const user = data.me
 
           return (
-            <Grid container className={classes.root} spacing={16}>
-              <Grid item xs={12}>
-                <MainInfoDisplay user={user} />
-              </Grid>
-              <Grid item xs={2} className={classes.grid} />
-              <Grid item xs={9} className={classes.grid}>
-                <QaDisplay user={user} />
-              </Grid>
-              <Grid item xs={12} className={classes.grid}>
-                <BadgesDisplay user={user} />
-              </Grid>
-              <Grid item xs={2} className={classes.grid} />
+            <Grid container className={classes.container}>
+              <Table className={classes.table}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>
+                      <Typography variant="display3" className={classes.title}>
+                        {`${user.name}'s Profile`}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+              </Table>
+              <MainInfoDisplay user={user} />
+              <QaDisplay user={user} />
+              <BadgesDisplay user={user} />
             </Grid>
-          );
+          )
         }}
       </Query>
-    );
+    )
   }
 }
 
-export default withStyles(styles)(DisplayAccount);
+export default withStyles(styles)(DisplayAccount)
