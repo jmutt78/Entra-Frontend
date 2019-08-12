919734d9be21023294322df6496891f9f1489f3a
Felix Volny <github@volny.co>
2019-08-12 19:45:54 +0700

    configure next to import svgs

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const withCSS = require('@zeit/next-css')
 
 module.exports = withCSS({
-  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 })
