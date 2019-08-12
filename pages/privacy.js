215b5b01ca6af50e1c79cd6bb7964587ecb63989
Felix Volny <github@volny.co>
2019-08-12 22:09:24 +0700

    privacy, team, and terms pages

---
+++ pages/privacy.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Table from '@material-ui/core/Table'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+
+const styles = ({ layout, palette }) => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  table: {},
+  title: {
+    fontSize: '40px',
+    textAlign: 'Left',
+    color: 'rgba(0, 0, 0, 0.87)',
+  },
+})
+
+const Terms = ({ classes }) => {
+  return (
+    <Grid container className={classes.container}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <Typography variant="display3" className={classes.title}>
+                Privacy Policy
+              </Typography>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+    </Grid>
+  )
+}
+
+export default withStyles(styles)(Terms)
+
