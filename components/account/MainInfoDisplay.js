cabad17b513f65093ee44ac5e7ea1efff5a6a2c4
Felix Volny <github@volny.co>
2019-08-19 19:50:55 +0700

    blank layout MainInfoDisplay

--- components/account/MainInfoDisplay.js
+++ components/account/MainInfoDisplay.js
@@ -1,119 +1,84 @@
-import React, { Component } from "react";
-import { format, parseISO } from "date-fns";
-import { Query } from "react-apollo";
-import Link from "next/link";
-import Avatar from "@material-ui/core/Avatar";
-import { withStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
+import React, { Component } from 'react'
+import { format, parseISO } from 'date-fns'
+import { Query } from 'react-apollo'
+import Link from 'next/link'
+import Avatar from '@material-ui/core/Avatar'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
 
-import { CURRENT_USER_QUERY } from "../auth/User";
+import { CURRENT_USER_QUERY } from '../auth/User'
 
 const styles = theme => ({
   bigAvatar: {
     width: 120,
-    height: 120
+    height: 120,
   },
   grid: {
-    margin: theme.spacing.unit
+    margin: theme.spacing.unit,
   },
   root: {
     margin: theme.spacing.unit,
-    marginTop: 40
+    marginTop: 40,
   },
   divider: {
-    marginTop: theme.spacing.unit * 5
+    marginTop: theme.spacing.unit * 5,
   },
   about: {
     fontSize: 17,
-    color: "grey"
-  }
-});
+    color: 'grey',
+  },
+})
 
 class MainInfoDisplay extends Component {
-  handleImage(user, classes) {
-    if (user.image == null || user.image == "") {
-      return (
-        <div>
-          <Avatar className={classes.bigAvatar}>{user.name[0]}</Avatar>
-        </div>
-      );
-    }
-    return (
-      <div>
-        <Avatar
-          alt="Remy Sharp"
-          src={user.image}
-          className={classes.bigAvatar}
-        />
-      </div>
-    );
-  }
+  handleImage(user, classes) {}
 
   handleEdit(me, user, classes) {
-    if (me.id === user.id) {
-      return (
-        <div>
-          <Typography>
-            <Link href="/account/editaccount">
-              <a style={{ textDecoration: "none", color: "grey" }}>
-                EDIT ACCOUNT INFO
-              </a>
-            </Link>
-          </Typography>
-        </div>
-      );
-    }
-    return <div />;
+    return <div />
   }
 
   render() {
     return (
       <Query query={CURRENT_USER_QUERY}>
         {({ data, loading }) => {
-          if (loading) return <p>Loading...</p>;
-          const { classes } = this.props;
+          if (loading) return <p>Loading...</p>
+          const { classes } = this.props
 
-          const user = this.props.user;
-          const me = data.me;
+          const user = this.props.user
+          const me = data.me
 
-          const dateToFormat = this.props.user.createdAt;
+          const dateToFormat = this.props.user.createdAt
           return (
-            <Grid container className={classes.root} spacing={16}>
-              <Grid item xs={2} className={classes.grid} />
-              <Grid item xs={1} className={classes.grid}>
-                {this.handleImage(user, classes)}
-              </Grid>
-              <Grid item xs={4} className={classes.grid}>
-                <Typography variant="h4">{user.name}</Typography>
-                <Typography variant="h6">{user.display}</Typography>
-                <Typography variant="subheading">
-                  Location: {user.location}
-                </Typography>
-                <Typography variant="subheading">
-                  Industry: {user.industry}
-                </Typography>
+            <>
+              {user.image ? (
+                <Avatar src={user.image} className={classes.bigAvatar} />
+              ) : (
+                <Avatar className={classes.bigAvatar}>{user.name[0]}</Avatar>
+              )}
+
+              <Typography variant="h4">{user.name}</Typography>
+              <Typography variant="h6">{user.display}</Typography>
+              <Typography variant="subheading">Location: {user.location}</Typography>
+              <Typography variant="subheading">Industry: {user.industry}</Typography>
+              <Typography>Member Since {format(parseISO(dateToFormat), 'MMMM dd, yyyy')}</Typography>
+
+              {me.id === user.id ? (
                 <Typography>
-                  Member Since {format(parseISO(dateToFormat), "MMMM dd, yyyy")}
+                  <Link href="/account/editaccount">
+                    <a style={{ textDecoration: 'none', color: 'grey' }}>EDIT ACCOUNT INFO</a>
+                  </Link>
                 </Typography>
-              </Grid>
-              <Grid item className={classes.grid} />
-              <Grid item className={classes.grid}>
-                {this.handleEdit(me, user, classes)}
-              </Grid>
-              <Grid item xs={2} className={classes.grid} />
-              <Grid item xs={2} className={classes.grid} />
-              <Grid item xs={7} className={classes.grid}>
-                <Typography className={classes.about}>{user.about}</Typography>
-                <Divider className={classes.divider} variant="middle" />
-              </Grid>
-            </Grid>
-          );
+              ) : null}
+
+              <Typography className={classes.about}>{user.about}</Typography>
+              <Divider className={classes.divider} variant="middle" />
+            </>
+          )
         }}
       </Query>
-    );
+    )
   }
 }
 
-export default withStyles(styles)(MainInfoDisplay);
+export default withStyles(styles)(MainInfoDisplay)
