b34fb32ee5efbba74c5c46ea84a4147894425216
jmutt78 <jmcintosh@docusites.com>
2019-08-21 10:28:00 -0700

    -Added Terms and Privacy

---
+++ components/terms-conditions/index.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+
+import gql from "graphql-tag";
+import { format, parseISO } from "date-fns";
+
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+import Error from "../ErrorMessage";
+import Head from "next/head";
+
+const TERMS_QUERY = gql`
+  query TERMS_QUERY($id: ID!) {
+    page(id: $id) {
+      content
+      title
+      id
+    }
+  }
+`;
+
+const styles = theme => ({
+  grid: {
+    margin: theme.spacing.unit
+  },
+  root: {
+    margin: theme.spacing.unit,
+    marginTop: 40
+  },
+  title: {
+    marginTop: theme.spacing.unit * 5,
+    marginBottom: theme.spacing.unit * 5,
+    color: "#343434",
+    flexGrow: 1,
+    textAlign: "center"
+  },
+  content: {
+    maxWidth: "100%",
+    alignItems: "left"
+  },
+  featured: { width: "800px", maxWidth: "100%", flexGrow: 1 }
+});
+
+class TermsConditions extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Query
+        query={TERMS_QUERY}
+        variables={{
+          id: "cGFnZToyMDkw"
+        }}
+        context={{ clientName: "second" }}
+      >
+        {({ data: { page }, loading }) => {
+          console.log(page);
+          if (loading) return <p>Loading...</p>;
+          function createMarkup() {
+            return { __html: createMarkup() };
+          }
+          return (
+            <Grid container className={classes.root} spacing={16}>
+              <Grid item xs={3} />
+
+              <Grid item xs={6} className={classes.grid}>
+                <Typography className={classes.title} variant="h2">
+                  {page.title}
+                </Typography>
+
+                <Typography variant="h6" color="textSecondary" display="block">
+                  <div
+                    className={classes.content}
+                    dangerouslySetInnerHTML={{
+                      __html: page.content
+                    }}
+                  />
+                </Typography>
+              </Grid>
+            </Grid>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default withStyles(styles)(TermsConditions);
--- pages/terms.js
+++ pages/terms.js
@@ -1,41 +1,6 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
-import { withStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import Table from '@material-ui/core/Table'
-import TableCell from '@material-ui/core/TableCell'
-import TableHead from '@material-ui/core/TableHead'
-import TableRow from '@material-ui/core/TableRow'
+import React from "react";
+import TermsConditions from "../components/terms-conditions";
 
-const styles = ({ layout, palette }) => ({
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  table: {},
-  title: {
-    fontSize: '40px',
-    textAlign: 'Left',
-    color: 'rgba(0, 0, 0, 0.87)',
-  },
-})
+const Terms = props => <TermsConditions />;
 
-const Terms = ({ classes }) => {
-  return (
-    <Grid container className={classes.container}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>
-              <Typography variant="display3" className={classes.title}>
-                Terms and Conditions
-              </Typography>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-      </Table>
-    </Grid>
-  )
-}
-
-export default withStyles(styles)(Terms)
+export default Terms;
