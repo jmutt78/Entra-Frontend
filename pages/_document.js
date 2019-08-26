0a542c0abf932e2b12fca58e1a6aed018c3b57f9
Felix Volny <github@volny.co>
2019-08-26 19:28:21 +0700

    fix favicon

--- pages/_document.js
+++ pages/_document.js
@@ -26,13 +26,12 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
 
-          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png">
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png">
-          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png">
-          <link rel="manifest" href="/static/favicon/site.webmanifest">
-          <meta name="msapplication-TileColor" content="#da532c">
-          <meta name="theme-color" content="#ffffff">
-
+          <link rel="apple-touch-icon" sizes="180x180" href="../static/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="../static/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="../static/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="../static/favicon/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body style={containerStyles}>
           <Main style={containerStyles} />
--- snippets/_documents.js
+++ snippets/_documents.js
@@ -33,12 +33,12 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
 
-          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png">
-          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png">
-          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png">
-          <link rel="manifest" href="/static/favicon/site.webmanifest">
-          <meta name="msapplication-TileColor" content="#da532c">
-          <meta name="theme-color" content="#ffffff">
+          <link rel="apple-touch-icon" sizes="180x180" href="../static/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="../static/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="../static/favicon/favicon-16x16.png" />
+          <link rel="manifest" href="../static/favicon/site.webmanifest" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
 
         </Head>
         <body style={containerStyles}>
