a5497c6b7f00ae8fd419047d42bcf3ff93967514
Felix Volny <github@volny.co>
2019-08-26 15:28:36 +0700

    responsive badges title and 'no badge' note

--- components/account/BadgesDisplay.js
+++ components/account/BadgesDisplay.js
@@ -122,11 +122,11 @@ export function BadgeItem({ type, classes }) {
 
 export function BadgesList({ badges, classes }) {
   const yourBadges = pickBy(badges, value => value === true)
-  const badgeKeys = Object.keys(yourBadges)
-  // const badgeKeys = Object.keys(badges)
+  // const badgeKeys = Object.keys(yourBadges)
+  const badgeKeys = Object.keys(badges)
   if (!badgeKeys.length) {
     return (
-      <Typography variant="h6" align="center">
+      <Typography variant="h6" style={{padding: '0 0 30px 50px'}}>
         No bages yet
       </Typography>
     )
