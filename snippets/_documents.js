cdf2cc8714d022ccf10e37256df7f6e2616aab9b
Felix Volny <github@volny.co>
2019-08-26 16:05:27 +0700

    add favicon, webmanifest, browserconfig, touch icons, etc

--- snippets/_documents.js
+++ snippets/_documents.js
@@ -32,6 +32,14 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i&display=swap"
             rel="stylesheet"
           />
+
+          <link rel="apple-touch-icon" sizes="180x180" href="/static/favicon/apple-touch-icon.png">
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon/favicon-32x32.png">
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon/favicon-16x16.png">
+          <link rel="manifest" href="/static/favicon/site.webmanifest">
+          <meta name="msapplication-TileColor" content="#da532c">
+          <meta name="theme-color" content="#ffffff">
+
         </Head>
         <body style={containerStyles}>
           <Main style={containerStyles} />
