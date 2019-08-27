76230613339bb72db89b2b30f2f02f06b5bfc3d4
Felix Volny <github@volny.co>
2019-08-27 13:10:52 +0700

    pagination moves to left on small screens

--- components/pagination/index.js
+++ components/pagination/index.js
@@ -1,59 +1,49 @@
-import React from "react";
-import gql from "graphql-tag";
-import { Query } from "react-apollo";
+import React from 'react'
+import { Query } from 'react-apollo'
 
-import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/core/styles";
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
 
-import PaginationActions from "./PaginationActions";
-import { perPage } from "../../config.js";
-import Error from "../ErrorMessage";
+import PaginationActions from './PaginationActions'
+import { perPage } from '../../config.js'
+import Error from '../ErrorMessage'
+
+import './index.css'
 
 const styles = ({ palette }) => ({
-  paginationContainer: {
-    width: "100%",
-    display: "flex",
-    justifyContent: "flex-end",
-    padding: "15px 0 0 0"
-  },
   text: {
     fontSize: 16,
-    color: "#2d3436",
-    textDecoration: "none",
-    fontWeight: 500
+    color: '#2d3436',
+    textDecoration: 'none',
+    fontWeight: 500,
   },
   subContainer: {
-    display: "flex",
-    alignItems: "center"
-  }
-});
+    display: 'flex',
+    alignItems: 'center',
+  },
+})
 
 function Pagination({ filter, page, classes, query, connectionKey }) {
   return (
     <Query query={query} variables={{ filter: filter }}>
       {({ data, loading, error }) => {
-        if (loading) return <p>Loading...</p>;
-        if (error) return <Error error={error} />;
+        if (loading) return <p>Loading...</p>
+        if (error) return <Error error={error} />
 
-        const count = data[connectionKey].aggregate.count;
-        const pages = Math.ceil(count / perPage);
+        const count = data[connectionKey].aggregate.count
+        const pages = Math.ceil(count / perPage)
 
         return (
-          <div
-            className={classes.paginationContainer}
-            style={pages <= 1 ? { display: "none" } : {}}
-          >
+          <div className="paginationContainer" style={pages <= 1 ? { display: 'none' } : {}}>
             <Typography className={classes.subContainer}>
-              <span
-                className={classes.text}
-              >{`Showing page ${page} of ${pages}`}</span>
+              <span className={classes.text}>{`Showing page ${page} of ${pages}`}</span>
             </Typography>
             <PaginationActions page={page} pages={pages} />
           </div>
-        );
+        )
       }}
     </Query>
-  );
+  )
 }
 
-export default withStyles(styles)(Pagination);
+export default withStyles(styles)(Pagination)
