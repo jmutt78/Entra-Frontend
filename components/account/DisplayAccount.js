30ef7b782a1e75994fdd72d50d4ab6dabf04e3d7
Felix Volny <github@volny.co>
2019-08-27 12:55:40 +0700

    fix account alignment on big screens

--- components/account/DisplayAccount.js
+++ components/account/DisplayAccount.js
@@ -3,7 +3,6 @@ import { Query } from 'react-apollo'
 
 import BadgesDisplay from './BadgesDisplay'
 import Error from './../ErrorMessage.js'
-import Grid from '@material-ui/core/Grid'
 import MainInfoDisplay from './MainInfoDisplay'
 import QaDisplay from './QaDisplay'
 import Table from '@material-ui/core/Table'
@@ -16,9 +15,14 @@ import { withStyles } from '@material-ui/core/styles'
 import { CURRENT_USER_QUERY } from '../auth/User'
 
 const styles = theme => ({
+  root: {
+    width: '100%',
+    maxWidth: 1200,
+  },
   container: {
     display: 'flex',
-    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   title: {
     fontSize: '40px',
@@ -54,24 +58,29 @@ class DisplayAccount extends Component {
           const user = data.me
 
           return (
-            <Grid container className={classes.container}>
-              <Table className={classes.table}>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>
-                      <Typography variant="h3" className={classes.title}>
-                        {`${user.name}'s Profile`}
-                      </Typography>
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-              </Table>
-              <div className={classes.contentContainer}>
-                <MainInfoDisplay user={user} />
-                <QaDisplay user={user} />
-                <BadgesDisplay user={user} />
+            <div className={classes.root}>
+              <div container className={classes.container}>
+
+                <Table className={classes.table}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>
+                        <Typography variant="h3" className={classes.title}>
+                          {`${user.name}'s Profile`}
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  </TableHead>
+                </Table>
+
+                <div className={classes.contentContainer}>
+                  <MainInfoDisplay user={user} />
+                  <QaDisplay user={user} />
+                  <BadgesDisplay user={user} />
+                </div>
+
               </div>
-            </Grid>
+            </div>
           )
         }}
       </Query>
