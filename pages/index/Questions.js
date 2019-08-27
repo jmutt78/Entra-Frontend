393988c3b798d993fd4c1d9b8ababab4a0ede23b
Felix Volny <github@volny.co>
2019-08-28 01:21:57 +0700

    factor out landing questions component

---
+++ pages/index/Questions.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Query } from 'react-apollo'
+import QuestionList from '../../components/question-list'
+import Error from '../../components/ErrorMessage'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import query from './query'
+
+export default () => (
+  <Query query={query}>
+    {({ data, loading, error }) => {
+      if (loading) return <CircularProgress style={{ margin: 20 }} />
+      if (error) return <Error error={error} />
+      return <QuestionList questions={data.questions} filter={'all'} page="1" name={'Latest Questions'} />
+    }}
+  </Query>
+)
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,22 +1,8 @@
-import React, { Component } from 'react'
-import { Query } from 'react-apollo'
-import QuestionList from '../../components/question-list'
-import Error from '../../components/ErrorMessage'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import query from './query'
+import React from 'react'
+import Questions from './Questions'
 
-class Questions extends Component {
-  render() {
-    return (
-      <Query query={query}>
-        {({ data, loading, error }) => {
-          if (loading) return <CircularProgress style={{ margin: 20 }} />
-          if (error) return <Error error={error} />
-          return <QuestionList questions={data.questions} filter={'all'} page="1" name={'Latest Questions'} />
-        }}
-      </Query>
-    )
-  }
+const Landingpage = () => {
+  return <Questions />
 }
 
-export default Questions
+export default Landingpage
