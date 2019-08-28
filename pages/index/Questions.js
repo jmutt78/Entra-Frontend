1b708b83d692815f230eeab6f8d7c790cd4b511f
Felix Volny <github@volny.co>
2019-08-28 11:24:53 +0700

    give padding back individually

--- pages/index/Questions.js
+++ pages/index/Questions.js
@@ -5,13 +5,21 @@ import Error from '../../components/ErrorMessage'
 import CircularProgress from '@material-ui/core/CircularProgress'
 import query from './query'
 
+const styles = {
+  container: {
+    padding: 20,
+    // TEMP
+    overflowX: 'hidden',
+  },
+}
+
 export default () => (
   <Query query={query}>
     {({ data, loading, error }) => {
       if (loading) return <CircularProgress style={{ margin: 20 }} />
       if (error) return <Error error={error} />
       return (
-        <div style={{ padding: '20px 0' }}>
+        <div style={styles.container}>
           <QuestionList questions={data.questions} filter={'all'} page="1" name={'Latest Questions'} />
         </div>
       )
