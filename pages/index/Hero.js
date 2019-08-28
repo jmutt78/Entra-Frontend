13edf387617f7feabb67d0acc634e215ad4087d1
Felix Volny <github@volny.co>
2019-08-28 11:06:18 +0700

    hero text

--- pages/index/Hero.js
+++ pages/index/Hero.js
@@ -3,6 +3,11 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className="heroContainer">
+      <div className="heroContent">
+        <h1 className="heroHeader">Entra</h1>
+        <p className="heroText">Get inspired. Get help. Grow.</p>
+        <p className="heroText">Entra is currently in private beta. Signup to reseave your invitation</p>
+      </div>
     </div>
   )
 }
