670ac57c2e962c233903f0df0357b4704aea34a0
Felix Volny <github@volny.co>
2019-08-30 01:19:47 +0700

    grid sections styling

--- pages/index/Grid.js
+++ pages/index/Grid.js
@@ -5,13 +5,13 @@ const Grid = ({ image, head, sub, flexDirection, sections }) => {
     <div className="gridContainer" style={{ flexDirection }}>
       <div className="gridContent">
         <h3 className="gridHeader sans">{head}</h3>
-        <h4 className="gridSubHeader sans">{sub}</h4>
-        <hr className="sectionbreak" />
+        <h4 className="gridSubHeader serif">{sub}</h4>
+        <hr className="gridSectionbreak" />
         {sections.map(({ icon, head, body }, key) => (
-          <div key={key}>
-            <img src={icon} alt={head} />
-            <p className="gridSectionHead">{head}</p>
-            <p className="gridSectionBody">{body}</p>
+          <div key={key} style={{padding: '1rem 0'}}>
+            <img src={icon} alt={head} style={{maxWidth: 60}} />
+            <p className="gridSectionHead sans">{head}</p>
+            <div className="gridSectionBody serif">{body}</div>
           </div>
         ))}
       </div>
