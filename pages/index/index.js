93b324cc808a4ae3680324a0c373fb1ead2b491d
Felix Volny <github@volny.co>
2019-08-29 22:49:01 +0700

    grid structure, images + WP data

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 import Areyou from './Areyou'
+import Ask from './Ask'
+import Clout from './Clout'
+import Grid from './Grid'
 import Hero from './Hero'
+import Inspired from './Inspired'
 import Questions from './Questions'
 import Why from './Why'
 
@@ -13,6 +17,26 @@ const Landingpage = () => {
       <Hero />
       <Why />
       <Questions />
+      <Grid image="/static/inspired.jpg" head="Get Inspired" sub="Learn from fellow entrepreneurs.">
+        <Inspired />
+      </Grid>
+      <Grid image="/static/ask.jpeg" head="Ask a Question" sub="Ask anything related to business.">
+        <Ask />
+      </Grid>
+      <Grid
+        image="/static/answers.jpg"
+        head="Get Answers"
+        sub="Crowdsource answers from other entrepreneurs and advisors."
+      >
+        <Answers />
+      </Grid>
+      <Grid
+        image="/static/clout.jpg"
+        head="Gain clout and notoriety"
+        sub="Become an expert, help other Entras and grow your network."
+      >
+        <Clout />
+      </Grid>
       <Areyou />
     </div>
   )
