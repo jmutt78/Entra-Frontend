096c7a2f609b3bff18ad214ed270543f328a84ca
Felix Volny <github@volny.co>
2019-08-30 02:06:17 +0700

    call to action section with conditional buttons and text

--- pages/index/callToAction.js
+++ pages/index/callToAction.js
@@ -1,26 +1,31 @@
 import React from 'react'
-import Typography from "@material-ui/core/Typography"
+import Typography from '@material-ui/core/Typography'
 import { withRouter } from 'next/router'
+import User from '../../components/auth/User'
 
-const Why = () => {
+const Why = ({ router }) => {
   return (
-    <div className="whyContainer">
-      <div className="whyContent">
-        <h2 className="whyHeader sans">Join us today</h2>
-              <button
-                className="heroSignupButton"
-                onClick={e => {
-                  e.preventDefault()
-                  e.stopPropagation()
-                  router.push({
-                    pathname: '/signup',
-                  })
-                }}
-              >
-                <Typography className="heroSignupButtonText">SIGNUP NOW</Typography>
-              </button>
-      </div>
-    </div>
+    <User>
+      {({ data: { me } }) => (
+        <div className="whyContainer">
+          <div className="callToActionContent">
+            <h2 className="whyHeader sans">{me ? 'Get involved' : 'Join us today'}</h2>
+            <button
+              className="heroSignupButton"
+              onClick={e => {
+                e.preventDefault()
+                e.stopPropagation()
+                router.push({
+                  pathname: me ? '/qa' : '/signup',
+                })
+              }}
+            >
+              <Typography className="heroSignupButtonText"> {me ? 'ASK A QUESTION' : 'SIGNUP NOW'}</Typography>
+            </button>
+          </div>
+        </div>
+      )}
+    </User>
   )
 }
 
