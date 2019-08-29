d8fd8282aaf4767bb83a9a6d6a699c79b888cae7
Felix Volny <github@volny.co>
2019-08-29 22:04:16 +0700

    signup button in hero showing only to logged out users

--- pages/index/Hero.js
+++ pages/index/Hero.js
@@ -1,15 +1,38 @@
 import React from 'react'
 
-const Hero = () => {
+import { withRouter } from 'next/router'
+
+import User from '../../components/auth/User'
+
+const Hero = ({ router }) => {
   return (
-    <div className="heroContainer">
-      <div className="heroContent">
-        <h1 className="heroHeader sans">Entra</h1>
-        <p className="heroText serif">Get inspired. Get help. Grow.</p>
-        <p className="heroText serif">Entra is currently in private beta. Signup to reserve your invitation</p>
-      </div>
-    </div>
+    <User>
+      {({ data: { me } }) => (
+        <div className="heroContainer">
+          <div className="heroContent">
+            <h1 className="heroHeader sans">Entra</h1>
+            <p className="heroText serif">Get inspired. Get help. Grow.</p>
+            <p className="heroText serif">
+              Entra is currently in private beta. Signup to reserve your invitation
+            </p>
+            {me ? null : (
+              <button
+                onClick={e => {
+                  e.preventDefault()
+                  e.stopPropagation()
+                  router.push({
+                    pathname: '/signup',
+                  })
+                }}
+              >
+                Signup
+              </button>
+            )}
+          </div>
+        </div>
+      )}
+    </User>
   )
 }
 
-export default Hero
+export default withRouter(Hero)
