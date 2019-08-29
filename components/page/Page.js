6f620ac066f2014c601cf174939fc9debfbc6eb5
jmutt78 <jmcintosh@docusites.com>
2019-08-29 06:31:03 -0700

    -

--- components/page/Page.js
+++ components/page/Page.js
@@ -1,41 +1,42 @@
-import React from 'react'
-import Header from '../header'
-import Meta from '../meta/Meta.js'
-import Footer from '../footer'
-import { withStyles } from '@material-ui/core/styles'
+import React from "react";
+import Header from "../header";
+import Meta from "../meta/Meta.js";
+import Footer from "../footer";
+import { withStyles } from "@material-ui/core/styles";
+import { withRouter } from "next/router";
 
-import './Page.css'
+import "./Page.css";
 
 const styles = ({ layout }) => {
   return {
     root: {
       width: "100%", //layout.width,
       height: "100%", //layout.height,
-      display: 'flex',
-      flexDirection: 'column',
+      display: "flex",
+      flexDirection: "column"
     },
     contentContainer: {
-      display: 'flex',
+      display: "flex",
       flex: 1,
 
       minHeight: layout.contentMinHeight,
-      overflowX: 'scroll',
+      overflowX: "scroll"
+    }
+  };
+};
 
-    },
-  }
-}
-
-const Page = ({ children, classes }) => {
+const Page = ({ children, classes, router }) => {
+  const isLanding = router.pathname === "/";
   return (
     <div className={classes.root}>
       <Meta />
       <Header />
-      <div className="contentContainerPadding">
+      <div className={isLanding ? "noPadding" : "contentContainerPadding"}>
         <div className={classes.contentContainer}>{children}</div>
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default withStyles(styles)(Page)
+export default withRouter(withStyles(styles)(Page));
