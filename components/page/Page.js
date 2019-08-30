5b66019c3679875c6b9e068dd73b0c9fab59957c
jmutt78 <jmcintosh@docusites.com>
2019-08-30 10:20:01 -0700

    -added drift

--- components/page/Page.js
+++ components/page/Page.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Header from '../header'
-import Meta from '../meta/Meta.js'
-import Footer from '../footer'
-import { withStyles } from '@material-ui/core/styles'
-import { withRouter } from 'next/router'
+import React from 'react';
+import Header from '../header';
+import Meta from '../meta/Meta.js';
+import Footer from '../footer';
+import { withStyles } from '@material-ui/core/styles';
+import { withRouter } from 'next/router';
 import classNames from 'classnames';
+import Drift from 'react-driftjs';
 
-import './Page.css'
+import './Page.css';
 
 const styles = ({ layout }) => {
   return {
@@ -14,28 +15,34 @@ const styles = ({ layout }) => {
       width: '100%', //layout.width,
       height: '100%', //layout.height,
       display: 'flex',
-      flexDirection: 'column',
+      flexDirection: 'column'
     },
     contentContainer: {
       display: 'flex',
       flex: 1,
-      minHeight: layout.contentMinHeight,
-    },
-  }
-}
+      minHeight: layout.contentMinHeight
+    }
+  };
+};
 
 const Page = ({ children, classes, router }) => {
-  const isLanding = router.pathname === '/'
+  const isLanding = router.pathname === '/';
   return (
     <div className={classes.root}>
       <Meta />
       <Header />
       <div className={isLanding ? 'noPadding' : 'contentContainerPadding'}>
-        <div className={classNames(classes.contentContainer, 'contentContainer')}>{children}</div>
+        <div
+          className={classNames(classes.contentContainer, 'contentContainer')}
+        >
+          {children}
+        </div>
+        <Drift appId="rz4xagciytry" />
       </div>
+
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default withRouter(withStyles(styles)(Page))
+export default withRouter(withStyles(styles)(Page));
