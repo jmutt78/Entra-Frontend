1a7bd265e4bde985f0a9373f6c5b959dc46bc319
anienhaus-woodridge <54550167+anienhaus-woodridge@users.noreply.github.com>
2019-08-30 13:23:50 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:4444`;
+//export const endpoint = `http://localhost:4444`;
+export const endpoint = 'http://Entraio-env.iimbrtpti9.us-east-1.elasticbeanstalk.com';
 export const blogurl = `http://entra.flywheelsites.com/graphql`;
 export const perPage = 10;
