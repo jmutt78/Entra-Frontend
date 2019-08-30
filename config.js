2df7ee2efd86442c79255642776797d0be4c1c79
jmutt78 <jmcintosh@docusites.com>
2019-08-30 07:02:41 -0700

    -

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
 export const blogurl = `http://entra.flywheelsites.com/graphql`;
-export const perPage = 2;
+export const perPage = 10;
