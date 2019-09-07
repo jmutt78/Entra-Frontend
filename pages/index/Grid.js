465d3991ddd36739baba2c69925e90236e01b909
Felix Volny <github@volny.co>
2019-09-08 00:25:57 +0700

    swap flexDirection for alignment for responsively changing flex

--- pages/index/Grid.js
+++ pages/index/Grid.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-const Grid = ({ image, head, sub, flexDirection, sections }) => {
+const Grid = ({ image, head, sub, alignment, sections }) => {
   return (
-    <div className="gridContainer" style={{ flexDirection }}>
+    <div
+      className={`gridContainer ${
+        alignment === 'left' ? 'gridContainerLeft' : 'gridContainerRight'
+      }`}
+    >
       <div className="gridContent">
         <h3 className="gridHeader sans">{head}</h3>
         <h4 className="gridSubHeader serif">{sub}</h4>
         <hr className="gridSectionbreak" />
         {sections.map(({ icon, head, body }, key) => (
-          <div key={key} style={{padding: '1rem 0'}}>
-            <img src={icon} alt={head} style={{maxWidth: 60}} />
+          <div key={key} style={{ padding: '1rem 0' }}>
+            <img src={icon} alt={head} style={{ maxWidth: 60 }} />
             <p className="gridSectionHead sans">{head}</p>
             <div className="gridSectionBody serif">{body}</div>
           </div>
@@ -20,7 +24,7 @@ const Grid = ({ image, head, sub, flexDirection, sections }) => {
         style={{ backgroundImage: `url(${image})` }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Grid
+export default Grid;
