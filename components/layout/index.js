2d2db332e842650d6c1f7ea9cd706756d4c03ea8
Felix Volny <github@volny.co>
2019-10-09 18:54:49 +0200

    re-add pageContext

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,15 +1,30 @@
-import React from "react";
-import { initGA, logPageView } from "../../utils/analytics.js";
+import React, { useState, useEffect, createContext, useContext } from 'react';
+import { initGA, logPageView } from '../../utils/analytics.js';
 
-export default class Layout extends React.Component {
-  componentDidMount() {
+const PageContext = createContext();
+
+export const usePageContext = () => {
+  return useContext(PageContext);
+};
+
+export default ({ children }) => {
+  const [sortBy, setSortBy] = useState('default');
+  useEffect(() => {
     if (!window.GA_INITIALIZED) {
       initGA();
       window.GA_INITIALIZED = true;
     }
     logPageView();
-  }
-  render() {
-    return this.props.children
-  }
-}
+  }, []);
+
+  return (
+    <PageContext.Provider
+      value={{
+        setSortBy,
+        sortBy
+      }}
+    >
+      {children}
+    </PageContext.Provider>
+  );
+};
