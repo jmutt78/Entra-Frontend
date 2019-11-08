09299ef9086718ea09949103c75abab68d957b65
Felix Volny <github@volny.co>
2019-11-08 11:16:13 +0100

    complete welcome section layout

--- components/Welcome/index.js
+++ components/Welcome/index.js
@@ -16,41 +16,78 @@ const useStyles = makeStyles(({ layout, palette }) => ({
     padding: '0 5px'
   },
   subtitle: {
-    fontSize: '40px',
-    textAlign: 'Left',
+    fontSize: '30px',
     color: 'rgba(0, 0, 0, 0.87)',
-    lineHeight: '3rem'
+    lineHeight: '3rem',
+    paddingLeft: '1.2rem'
+  },
+  sectionTitle: {
+    fontSize: '30px',
+    fontWeight: 700,
+    color: 'rgba(0, 0, 0, 0.87)',
+    lineHeight: '3rem',
+    padding: '0.5rem 0 2rem 1.2rem'
+  },
+  paragraph: {
+    fontSize: '16px',
+    padding: '0 1rem',
+    maxWidth: 800
+  },
+  credits: {
+    padding: '0 1rem',
+    fontStyle: 'italic',
+    fontWeight: 500
+  },
+  noteContainer: {
+    marginTop: '1rem',
+    marginBottom: '2rem',
+    background: palette.secondary.main,
+    padding: '1rem 0.5rem',
+    borderRadius: 2
   }
 }));
 
 export default () => {
-  const { container, subtitle } = useStyles();
+  const {
+    container,
+    credits,
+    noteContainer,
+    paragraph,
+    sectionTitle,
+    subtitle
+  } = useStyles();
   return (
     <div>
       <Grid container className={container}>
         <PageHeader title={'Welcome To Entra!'} />
+        <div className={noteContainer}>
+          <Typography variant="h6" className={subtitle}>
+            A Note from our founder
+          </Typography>
 
-        <div>A Note from our founder-</div>
-        <div>
-          When I was starting out in business, it was tough to find a community
-          of people to advise me on what to do. I had to rely on the few mentors
-          in my life (mostly my father).{' '}
+          <p className={paragraph}>
+            When I was starting out in business, it was tough to find a
+            community of people to advise me on what to do. I had to rely on the
+            few mentors in my life (mostly my father).{' '}
+          </p>
+
+          <p className={paragraph}>
+            When I gained some experience and became more seasoned, I made it my
+            priority to help other entrepreneurs in their journey.{' '}
+          </p>
+
+          <p className={paragraph}>
+            Entra is a place for entrepreneurs to show off their expertise, help
+            other people, and strengthen our community. This is something I wish
+            I’d had when starting out and why I’m excited to share it with you.
+          </p>
+          <p className={credits}>- Justin McIntosh, CEO</p>
         </div>
-        <br></br>
-        <div>
-          When I gained some experience and became more seasoned, I made it my
-          priority to help other entrepreneurs in their journey.{' '}
-        </div>
-        <br></br>
-        <div>
-          Entra is a place for entrepreneurs to show off their expertise, help
-          other people, and strengthen our community. This is something I wish
-          I’d had when starting out and why I’m excited to share it with you.
-        </div>
-        <div>-Justin McIntosh, CEO</div>
-        <Typography variant="h6" className={subtitle}>
+
+        <Typography variant="h6" className={sectionTitle}>
           Watch the video below to learn how to use Entra!
         </Typography>
+
         <div className="videoWrapper">
           <iframe
             title="video"
