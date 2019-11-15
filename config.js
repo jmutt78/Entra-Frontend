2f199e850156a6262a0294ba2fda85aa7d4d96bc
Alex Musca <musca.alexandru@gmail.com>
2019-11-15 10:28:08 +0200

    Merge with upstream changes

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://localhost:4444`;
-//export const endpoint = 'https://api.entra.io';
+export const endpoint = 'https://api.entra.io';
 export const blogurl = `https://entra.flywheelsites.com/graphql`;
 export const perPage = 10;
--- utils/Mixpanel.js
+++ utils/Mixpanel.js
@@ -1,19 +1,19 @@
 import mixpanel from 'mixpanel-browser';
-mixpanel.init('///////');
+// mixpanel.init('59a5230bfd7a84ed31296264899ab6e6');
 
 let actions = {
   identify: id => {
-    mixpanel.identify(id);
+    // mixpanel.identify(id);
   },
   alias: id => {
-    mixpanel.alias(id);
+    // mixpanel.alias(id);
   },
   track: (name, props) => {
-    mixpanel.track(name, props);
+    // mixpanel.track(name, props);
   },
   people: {
     set: props => {
-      mixpanel.people.set(props);
+      // mixpanel.people.set(props);
     }
   }
 };
