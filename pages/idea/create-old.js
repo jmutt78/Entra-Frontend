3a05e97a08f3a44e7f0cb28626867044b436f1ac
Felix Volny <github@volny.co>
2019-11-17 20:00:51 +0800

    new stepper

---
+++ pages/idea/create-old.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import IdeaCreate from '../../components/create-idea/old';
+import Layout from '../../components/layout/index.js';
+
+const CreateIdea = props => (
+  <Layout>
+    <IdeaCreate />
+  </Layout>
+);
+
+export default CreateIdea;
