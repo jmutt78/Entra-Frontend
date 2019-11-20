50861a280ff4bbf3808492031510fd3716549057
Felix Volny <github@volny.co>
2019-11-20 15:49:04 +0800

    idea view sections Cards UI

--- components/idea-display/index.js
+++ components/idea-display/index.js
@@ -2,14 +2,14 @@ import Link from 'next/link';
 import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
-import { format, parseISO } from 'date-fns';
-// import { withRouter } from 'next/router';
 
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-// import { withStyles } from '@material-ui/core/styles';
 
 import PageHeader from '../PageHeader';
 import Error from './../ErrorMessage.js';
@@ -35,46 +35,78 @@ export const IDEAS_QUERY = gql`
 `;
 
 const usePageStyles = makeStyles(({ palette, spacing }) => ({
-  container: {
-    width: '100%',
-    maxWidth: 1000,
-    paddingTop: 5,
-    height: '100%',
-    minHeight: 'calc(100%)-80px-80px' //layout.contentMinHeight,
-  },
-  title: {
-    color: 'rgba(0, 0, 0, 0.87)',
-    display: 'flex',
-    alignItems: 'center'
-  },
-  titleText: {
-    fontSize: '33px',
-    lineHeight: '2.7rem',
-    fontWeight: 600,
-    letterSpacing: '-1px'
-  },
-  bodyText: {
-    whiteSpace: 'pre-wrap',
-    fontSize: '1rem'
-  },
-  detailContainer: {
-    background: '#f2f4ef',
-    padding: '3px 0 5px 0',
-    marginLeft: 15,
-    marginRight: 15
-  },
-  editButton: {}
+  // container: {
+  //   width: '100%',
+  //   maxWidth: 1000,
+  //   paddingTop: 5,
+  //   height: '100%',
+  //   minHeight: 'calc(100%)-80px-80px' //layout.contentMinHeight,
+  // },
+  // title: {
+  //   color: 'rgba(0, 0, 0, 0.87)',
+  //   display: 'flex',
+  //   alignItems: 'center'
+  // },
+  // titleText: {
+  //   fontSize: '33px',
+  //   lineHeight: '2.7rem',
+  //   fontWeight: 600,
+  //   letterSpacing: '-1px'
+  // },
+  // bodyText: {
+  //   whiteSpace: 'pre-wrap',
+  //   fontSize: '1rem'
+  // },
+  // detailContainer: {
+  //   background: '#f2f4ef',
+  //   padding: '3px 0 5px 0',
+  //   marginLeft: 15,
+  //   marginRight: 15
+  // },
 }));
 
+const useSectionStyles = makeStyles(({ palette }) => ({
+  card: {
+    minWidth: 275,
+    maxWidth: 800
+  },
+  title: {
+    fontSize: 15
+  },
+  content: {
+    fontSize: 20,
+    fontWeight: 400
+  },
+  buttonContainer: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    paddingRight: '1rem'
+  }
+}));
+
+const Section = ({ sectionTitle, sectionContent }) => {
+  const { card, title, content, buttonContainer } = useSectionStyles();
+
+  return (
+    <Card className={card}>
+      <CardContent>
+        <Typography className={title} color="textSecondary" gutterBottom>
+          {sectionTitle}
+        </Typography>
+        <Typography className={content}>{sectionContent}</Typography>
+      </CardContent>
+      <CardActions>
+        <div className={buttonContainer}>
+          <Button size="small">Edit</Button>
+        </div>
+      </CardActions>
+    </Card>
+  );
+};
+
 export default ({ idea, id }) => {
-  const {
-    container,
-    title,
-    titleText,
-    bodyText,
-    detailContainer,
-    editButton
-  } = usePageStyles();
+  const { container, bodyText, detailContainer, editButton } = usePageStyles();
 
   return (
     <Query
@@ -90,18 +122,7 @@ export default ({ idea, id }) => {
           <div className={container}>
             <PageHeader title={`Business Idea`} subTitle={businessIdea.idea} />
 
-            <div className="titleContainer">
-              <Typography variant="h6" className={title}>
-                <div className={titleText}>{}</div>
-              </Typography>
-            </div>
-
-            <div style={{ padding: '0 0 0 10px' }}>
-              Created{' '}
-              <span>
-                {format(parseISO(businessIdea.createdAt), 'MMMM dd, yyyy')}
-              </span>
-            </div>
+            <Section sectionTitle="Idea" sectionContent="Word of the Day" />
 
             <div className={detailContainer}>
               <div className="QuestionDetail-body">
