7e70e90dabc931539148fb28cd8876406754c95d
Felix Volny <github@volny.co>
2019-12-22 18:55:15 +0700

    useWindowSize hook, adapt small/full logo based on window size

--- components/new-header/Logo.js
+++ components/new-header/Logo.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import Link from 'next/link';
 import { makeStyles } from '@material-ui/core/styles';
 
-import Typography from '@material-ui/core/Typography';
+const useWindowSize = () => {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    const updateSize = () => {
+      setSize([window.innerWidth, window.innerHeight]);
+    };
+    window.addEventListener('resize', updateSize);
+    updateSize();
+    return () => window.removeEventListener('resize', updateSize);
+  }, []);
+  return size;
+};
 
 const useStyles = makeStyles({
   logo: {
@@ -17,9 +28,23 @@ const useStyles = makeStyles({
 
 export default () => {
   const classes = useStyles();
+  const [width] = useWindowSize();
+
   return (
     <Link href="/all">
-      <img src="/static/logo.png" className={classes.logo} alt="entra logo" />
+      {width > 1100 ? (
+        <img
+          src="/static/logo-full.png"
+          className={classes.logo}
+          alt="entra logo"
+        />
+      ) : (
+        <img
+          src="/static/logo-small.png"
+          className={classes.logo}
+          alt="entra logo"
+        />
+      )}
     </Link>
   );
 };
