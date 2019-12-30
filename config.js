5cbb4352e6b5a3eb2efb74c03704755b3c45924c
Felix Volny <github@volny.co>
2019-12-30 15:24:00 +0700

    switch api endpoint based on NODE_ENV

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 
-export const endpoint = `http://localhost:4444`;
-//export const endpoint = 'https://api.entra.io';
+export const endpoint = (env => {
+  switch (env) {
+    case 'development':
+      return `http://localhost:4444`;
+    default:
+      return 'https://api.entra.io';
+  }
+})(process.env.NODE_ENV);
 
 export const blogurl = `https://entra.flywheelsites.com/graphql`;
 export const perPage = 10;
